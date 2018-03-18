ff4a4113008630ea8ade2ffa3259372e54d9e9f2
nguardadov <nguardadov@gmail.com>
2018-03-18 17:59:17 -0600

    Ejemplo de uso

--- app/src/main/java/com/guardado/holamundo/MainActivity.java
+++ app/src/main/java/com/guardado/holamundo/MainActivity.java
@@ -2,13 +2,48 @@ package com.guardado.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Declaramos los controles que usaremos
+     //TextView
+    TextView txvMensaje;
+    //EditText
+    EditText txtUsuario;
+    //Button
+    Button btnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hola mundo
+
+        //buscaremos cada uno de los elementos para poder trabajarlos
+        this.txvMensaje = (TextView) findViewById(R.id.txvMensaje);
+        this.txtUsuario = (EditText) findViewById(R.id.txtUser);
+        this.btnClick = (Button) findViewById(R.id.btnClick);
+
+        //activaremos el evento de click en el boton
+        this.btnClick.setOnClickListener(this);
+    }
+
+
+    //Por medio de este metodo le daremos la funcionalidad al boton
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnClick:
+                //en una variable extreamos el contenido del edittext
+                String user = this.txtUsuario.getText().toString(); // lo convertimos a cadena
+                //cambiamos el conetenido del textview
+                this.txvMensaje.setText(user);
+
+                break;
+        }
     }
 }
